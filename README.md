cae78bdbf7c88c983c48dab6f731e2ad5d388c54
Sergey Trofimov <sergey@trofimov.name>
2022-09-08 12:31:31 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,16 @@
+# parsessor
+
+Parser combinators for Clojure(Script).
+
+[![Clojars Project](https://img.shields.io/clojars/v/com.github.strojure/parsessor.svg)](https://clojars.org/com.github.strojure/parsessor)
+
+## Motivation
+
+* Idiomatic and convenient API for parser combinators in Clojure and
+  ClojureScript.
+
+## Inspiration
+
+* [blancas/kern](https://github.com/blancas/kern)
+* [youngnh/parsatron](https://github.com/youngnh/parsatron)
+* [rm-hull/jasentaa](https://github.com/rm-hull/jasentaa)
---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject com.github.strojure/parsessor "0.0.0-SNAPSHOT"
+  :description "Parser combinators library for Clojure(Script)."
+  :url "https://github.com/strojure/parsessor"
+  :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies []
+
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]
+                                       [org.clojure/clojurescript "1.11.60"]]}
+             :dev,,,,, {:dependencies [;; clojurescript repl deps
+                                       [com.google.guava/guava "31.1-jre"]
+                                       ;; inspiration libs
+                                       [org.blancas/kern "1.1.0"]
+                                       [rm-hull/jasentaa "0.2.5"]
+                                       [the/parsatron "0.0.8"]]}}
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo" :sign-releases false}]])
