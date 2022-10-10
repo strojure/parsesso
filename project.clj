026b13eaf4f79e437d5dc503341f2e33f07b3568
serioga <github@sergey.trofimov.name>
2022-10-10 19:51:43 +0200

    Release version 1.0.191-beta

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/parsesso "1.0.184-beta"
+(defproject com.github.strojure/parsesso "1.0.191-beta"
   :description "Parser combinators library for Clojure(Script)."
   :url "https://github.com/strojure/parsesso"
   :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
