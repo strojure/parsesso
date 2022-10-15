db2f75cb090cadcbd9fed4e80be190be94856bf9
serioga <github@sergey.trofimov.name>
2022-10-15 13:59:42 +0200

    Rename ns aliases in benchmarks

--- test/perf/bench.clj
+++ test/perf/bench.clj
@@ -1,18 +1,18 @@
 (ns perf.bench
   "Some benchmarks between parsesso, kern and parsatron. There is no jasentaa
   here because it is very slow."
-  (:require [blancas.kern.core :as krn]
+  (:require [blancas.kern.core :as k]
             [strojure.parsesso.char :as char]
             [strojure.parsesso.parser :as p]
-            [the.parsatron :as prt]))
+            [the.parsatron :as t]))
 
 (set! *warn-on-reflection* true)
 
 ;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 
-(defn- prt-run
+(defn- t-run
   [p input]
-  (prt/run-parser p (prt/->InputState input (prt/->SourcePos 1 1))))
+  (t/run-parser p (t/->InputState input (t/->SourcePos 1 1))))
 
 (def ^:private -input-10000 (repeat 10000 :a))
 
@@ -26,13 +26,13 @@
 ;   Execution time lower quantile : 153,729903 ns ( 2,5%)
 ;   Execution time upper quantile : 217,875203 ns (97,5%)
 
-(krn/parse (krn/return :x) [])
+(k/parse (k/return :x) [])
 ;             Execution time mean : 233,470315 ns
 ;    Execution time std-deviation : 66,244027 ns
 ;   Execution time lower quantile : 178,201399 ns ( 2,5%)
 ;   Execution time upper quantile : 326,518209 ns (97,5%)
 
-(prt/run (prt/always :x) [])
+(t/run (t/always :x) [])
 ;             Execution time mean : 168,392753 ns
 ;    Execution time std-deviation : 68,636364 ns
 ;   Execution time lower quantile : 123,449569 ns ( 2,5%)
@@ -48,13 +48,13 @@
 ;   Execution time lower quantile : 172,453755 ns ( 2,5%)
 ;   Execution time upper quantile : 210,153699 ns (97,5%)
 
-(krn/parse (krn/fail :x) [])
+(k/parse (k/fail :x) [])
 ;             Execution time mean : 386,590746 ns
 ;    Execution time std-deviation : 156,097460 ns
 ;   Execution time lower quantile : 266,519628 ns ( 2,5%)
 ;   Execution time upper quantile : 640,785168 ns (97,5%)
 
-(prt-run (prt/never) [])
+(t-run (t/never) [])
 ;             Execution time mean : 841,250545 ns
 ;    Execution time std-deviation : 206,671857 ns
 ;   Execution time lower quantile : 703,388694 ns ( 2,5%)
@@ -70,13 +70,13 @@
 ;   Execution time lower quantile : 268,625666 ns ( 2,5%)
 ;   Execution time upper quantile : 307,169162 ns (97,5%)
 
-(krn/parse (krn/satisfy #(= \a %)) "abc")
+(k/parse (k/satisfy #(= \a %)) "abc")
 ;             Execution time mean : 245,984170 ns
 ;    Execution time std-deviation : 13,553994 ns
 ;   Execution time lower quantile : 235,005603 ns ( 2,5%)
 ;   Execution time upper quantile : 268,329750 ns (97,5%)
 
-(prt/run (prt/token #(= \a %)) "abc")
+(t/run (t/token #(= \a %)) "abc")
 ;             Execution time mean : 557,024259 ns
 ;    Execution time std-deviation : 14,359373 ns
 ;   Execution time lower quantile : 541,631508 ns ( 2,5%)
@@ -92,13 +92,13 @@
 ;   Execution time lower quantile : 471,832427 ns ( 2,5%)
 ;   Execution time upper quantile : 531,601202 ns (97,5%)
 
-(krn/parse (krn/token* "abc") "abc")
+(k/parse (k/token* "abc") "abc")
 ;             Execution time mean : 4,020720 µs
 ;    Execution time std-deviation : 429,048420 ns
 ;   Execution time lower quantile : 3,767589 µs ( 2,5%)
 ;   Execution time upper quantile : 4,754242 µs (97,5%)
 
-(prt/run (prt/string "abc") "abc")
+(t/run (t/string "abc") "abc")
 ;             Execution time mean : 2,212562 µs
 ;    Execution time std-deviation : 91,094400 ns
 ;   Execution time lower quantile : 2,126279 µs ( 2,5%)
@@ -114,7 +114,7 @@
 ;   Execution time lower quantile : 618,951019 ns ( 2,5%)
 ;   Execution time upper quantile : 641,954344 ns (97,5%)
 
-(krn/parse (krn/token- "abc") "ABC")
+(k/parse (k/token- "abc") "ABC")
 ;             Execution time mean : 5,063223 µs
 ;    Execution time std-deviation : 212,754488 ns
 ;   Execution time lower quantile : 4,915983 µs ( 2,5%)
@@ -131,10 +131,10 @@
 ;   Execution time upper quantile : 209,664373 µs (97,5%)
 
 (comment
-  (krn/parse (krn/token* -input-10000) -input-10000))
+  (k/parse (k/token* -input-10000) -input-10000))
 ; Execution error (StackOverflowError) at blancas.kern.core/>>=$fn
 
-(prt/run (prt/string -input-10000) -input-10000)
+(t/run (t/string -input-10000) -input-10000)
 ;             Execution time mean : 5,677465 ms
 ;    Execution time std-deviation : 961,844848 µs
 ;   Execution time lower quantile : 4,976587 ms ( 2,5%)
@@ -150,13 +150,13 @@
 ;   Execution time lower quantile : 915,594047 ns ( 2,5%)
 ;   Execution time upper quantile : 1,059000 µs (97,5%)
 
-(krn/parse (krn/many krn/letter) "abc")
+(k/parse (k/many k/letter) "abc")
 ;             Execution time mean : 1,911586 µs
 ;    Execution time std-deviation : 511,124107 ns
 ;   Execution time lower quantile : 1,646502 µs ( 2,5%)
 ;   Execution time upper quantile : 2,783604 µs (97,5%)
 
-(prt/run (prt/many (prt/letter)) "abc")
+(t/run (t/many (t/letter)) "abc")
 ;             Execution time mean : 2,599675 µs
 ;    Execution time std-deviation : 576,904794 ns
 ;   Execution time lower quantile : 2,193151 µs ( 2,5%)
@@ -172,7 +172,7 @@
 ;   Execution time lower quantile : 1,439323 µs ( 2,5%)
 ;   Execution time upper quantile : 1,680704 µs (97,5%)
 
-(krn/parse (krn/<+> (krn/many krn/letter)) "abc")
+(k/parse (k/<+> (k/many k/letter)) "abc")
 ;             Execution time mean : 5,568215 µs
 ;    Execution time std-deviation : 145,037838 ns
 ;   Execution time lower quantile : 5,459951 µs ( 2,5%)
@@ -188,13 +188,13 @@
 ;   Execution time lower quantile : 1,223376 ms ( 2,5%)
 ;   Execution time upper quantile : 1,426319 ms (97,5%)
 
-(krn/parse (krn/many (krn/satisfy #(= :a %))) -input-10000)
+(k/parse (k/many (k/satisfy #(= :a %))) -input-10000)
 ;             Execution time mean : 1,928105 ms
 ;    Execution time std-deviation : 62,373984 µs
 ;   Execution time lower quantile : 1,868339 ms ( 2,5%)
 ;   Execution time upper quantile : 2,024112 ms (97,5%)
 
-(prt/run (prt/many (prt/token #(= :a %))) -input-10000)
+(t/run (t/many (t/token #(= :a %))) -input-10000)
 ;             Execution time mean : 1,066323 sec
 ;    Execution time std-deviation : 159,363140 ms
 ;   Execution time lower quantile : 984,876092 ms ( 2,5%)
@@ -209,7 +209,7 @@
 ;   Execution time lower quantile : 893,890237 µs ( 2,5%)
 ;   Execution time upper quantile : 1,465919 ms (97,5%)
 
-(krn/parse (krn/skip-many (krn/satisfy #(= :a %))) -input-10000)
+(k/parse (k/skip-many (k/satisfy #(= :a %))) -input-10000)
 ;             Execution time mean : 1,416146 ms
 ;    Execution time std-deviation : 35,717820 µs
 ;   Execution time lower quantile : 1,379739 ms ( 2,5%)
@@ -227,17 +227,17 @@
 ;   Execution time lower quantile : 511,933832 ns ( 2,5%)
 ;   Execution time upper quantile : 624,550670 ns (97,5%)
 
-(krn/parse (krn/<|> (krn/fail "a")
-                    (krn/fail "a")
-                    (krn/return :x)) [])
+(k/parse (k/<|> (k/fail "a")
+                (k/fail "b")
+                (k/return :x)) [])
 ;             Execution time mean : 1,754808 µs
 ;    Execution time std-deviation : 148,221426 ns
 ;   Execution time lower quantile : 1,618505 µs ( 2,5%)
 ;   Execution time upper quantile : 1,924351 µs (97,5%)
 
-(prt/run (prt/choice (prt/never)
-                     (prt/never)
-                     (prt/always :x)) [])
+(t/run (t/choice (t/never)
+                 (t/never)
+                 (t/always :x)) [])
 ;             Execution time mean : 697,151006 ns
 ;    Execution time std-deviation : 165,879602 ns
 ;   Execution time lower quantile : 570,024598 ns ( 2,5%)
@@ -253,7 +253,7 @@
 ;   Execution time lower quantile : 196,685023 ns ( 2,5%)
 ;   Execution time upper quantile : 238,117212 ns (97,5%)
 
-(krn/parse (krn/<?> (krn/return :x) "x") [])
+(k/parse (k/<?> (k/return :x) "x") [])
 ;             Execution time mean : 222,587325 ns
 ;    Execution time std-deviation : 16,924812 ns
 ;   Execution time lower quantile : 205,615791 ns ( 2,5%)
@@ -269,13 +269,13 @@
 ;   Execution time lower quantile : 209,952763 ns ( 2,5%)
 ;   Execution time upper quantile : 262,847436 ns (97,5%)
 
-(krn/parse krn/eof " ")
+(k/parse k/eof " ")
 ;             Execution time mean : 1,428015 µs
 ;    Execution time std-deviation : 81,057937 ns
 ;   Execution time lower quantile : 1,352623 µs ( 2,5%)
 ;   Execution time upper quantile : 1,560179 µs (97,5%)
 
-(prt-run (prt/eof) " ")
+(t-run (t/eof) " ")
 ;             Execution time mean : 882,705676 ns
 ;    Execution time std-deviation : 46,738939 ns
 ;   Execution time lower quantile : 837,580307 ns ( 2,5%)
@@ -291,9 +291,9 @@
 ;   Execution time lower quantile : 6,900613 µs ( 2,5%)
 ;   Execution time upper quantile : 8,221736 µs (97,5%)
 
-(krn/parse (krn/>> (krn/token* "<!--")
-                   (krn/many-till krn/any-char (krn/<:> (krn/token* "-->"))))
-           "<!-- comment -->")
+(k/parse (k/>> (k/token* "<!--")
+               (k/many-till k/any-char (k/<:> (k/token* "-->"))))
+         "<!-- comment -->")
 ;             Execution time mean : 84,653453 µs
 ;    Execution time std-deviation : 2,870985 µs
 ;   Execution time lower quantile : 81,222728 µs ( 2,5%)
