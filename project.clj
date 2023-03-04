6c7590f6bdbfc13ff0da83709e280f7199266c71
serioga <github@sergey.trofimov.name>
2023-03-04 08:37:00 +0100

    Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/parsesso "1.0.253"
+(defproject com.github.strojure/parsesso "1.0.254-SNAPSHOT"
   :description "Parser combinators library for Clojure(Script)."
   :url "https://github.com/strojure/parsesso"
   :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
