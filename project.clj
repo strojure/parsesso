dc28d84775eee54b7eabc8552523180d9193727a
Sergey Trofimov <sergey@trofimov.name>
2023-06-06 15:54:45 +0200

    (build): release version `1.2.2+295`

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/parsesso "1.2.2-SNAPSHOT"
+(defproject com.github.strojure/parsesso "1.2.2+295"
   :description "Parser combinators library for Clojure(Script)."
   :url "https://github.com/strojure/parsesso"
   :license {:name "The Unlicense" :url "https://unlicense.org"}
